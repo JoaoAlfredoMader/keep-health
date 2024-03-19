97bd9f6eb88c192b7ab51e8093f75790c3379da6
joaoalfredo9287 <joaoalfredo229@gmail.com>
2024-03-18 21:51:33 -0300

    add components + login setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../app/login/login.component';
+import { CadastroComponent } from '../app/cadastro/cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
